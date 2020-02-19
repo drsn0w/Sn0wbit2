4d8b67c04f6ef46e64239e79e4befef0a6fc4d5e
Liam Crabbe <liam.crabbe.3@gmail.com>
2020-02-19 10:55:59 -0700

    update filters in accordance with TG API changes
    Telegram now sets message.text to NoneType for user added/removed messages, etc, so we need to check if message.text is none before we try to do anything with it

--- custom_filters.py
+++ custom_filters.py
@@ -2,11 +2,13 @@ from telegram.ext import BaseFilter
 
 class RedditFilter(BaseFilter):
     def filter(self, message):
-        return '/r/' in message.text
+        if message.text is not None:
+            return '/r/' in message.text
 
 class MentionFilter(BaseFilter):
     def filter(self, message):
-        return '@Sn0wBit2_bot' in message.text
+        if message.text is not None:
+            return '@Sn0wBit2_bot' in message.text
 
 class UserAddedFilter(BaseFilter):
     def filter(self, message):
@@ -26,12 +28,14 @@ class PrivateMessageFilter(BaseFilter):
 
 class InsultingWorldFilter(BaseFilter):
     def filter(self, message):
-        insulting_words = ["kys", "faggot", "stupid", "aids", "whore", "eat me", "suicide"]
-        return any(word in message.text for word in insulting_words) and '@Sn0wBit2_bot' in message.text
+        if message.text is not None:
+            insulting_words = ["kys", "faggot", "stupid", "aids", "whore", "eat me", "suicide"]
+            return any(word in message.text for word in insulting_words) and '@Sn0wBit2_bot' in message.text
 
 class NoUFilter(BaseFilter):
     def filter(self, message):
-        return 'no u' in message.text or 'No u' in message.text or 'NO U' in message.text
+        if message.text is not None:
+            return 'no u' in message.text or 'No u' in message.text or 'NO U' in message.text
 
 class ClearFilter(BaseFilter):
     def filter(self, message):
