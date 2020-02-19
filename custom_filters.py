65bacb514a59eeffec09354f16bb4f4f40bd8f54
Liam Crabbe <liam.crabbe.3@gmail.com>
2020-02-19 12:38:33 -0700

    custom_filters.py: more filter fixes
    More fixes due to changed bot API causing some filters not to return anything

--- custom_filters.py
+++ custom_filters.py
@@ -1,26 +1,24 @@
 from telegram.ext import BaseFilter
 
+class InGroupFilter(BaseFilter):
+    def filter(self, message):
+        return message.chat.type == 'group'
+
 class RedditFilter(BaseFilter):
     def filter(self, message):
-        if message.text is not None:
-            return '/r/' in message.text
+        return message.text is not None and '/r/' in message.text
 
 class MentionFilter(BaseFilter):
     def filter(self, message):
-        if message.text is not None:
-            return '@Sn0wBit2_bot' in message.text
+        return message.text is not None and '@Sn0wBit2_bot' in message.text
 
 class UserAddedFilter(BaseFilter):
     def filter(self, message):
-        return message.new_chat_members is not None
+        return len(message.new_chat_members) > 0
 
 class UserLeftFilter(BaseFilter):
     def filter(self, message):
         return message.left_chat_member is not None
-
-class InGroupFilter(BaseFilter):
-    def filter(self, message):
-        return message.from_user.id != message.chat_id
 
 class PrivateMessageFilter(BaseFilter):
     def filter(self, message):
@@ -31,12 +29,16 @@ class InsultingWorldFilter(BaseFilter):
         if message.text is not None:
             insulting_words = ["kys", "faggot", "stupid", "aids", "whore", "eat me", "suicide"]
             return any(word in message.text for word in insulting_words) and '@Sn0wBit2_bot' in message.text
+        else:
+            return False
 
 class NoUFilter(BaseFilter):
     def filter(self, message):
         if message.text is not None:
             return 'no u' in message.text or 'No u' in message.text or 'NO U' in message.text
+        else:
+            return False
 
 class ClearFilter(BaseFilter):
     def filter(self, message):
-        return message.text == "clear"
+        return message.text is not None and message.text == "clear"
